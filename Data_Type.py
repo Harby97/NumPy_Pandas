252b6f6fb69f7773af01089becea20ea2c96b34c
Harby Morales <ingharby@gmail.com>
2022-05-17 09:29:09 -0500

    Tipos de datos en los array y como cambiarlos

---
+++ Data_Type.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+list = [0,1,2,3,4,5,6,7,8,9]
+print (list)
+
+#lista ya te tipo array
+arr = np.array(list)
+print (arr)
+print(arr.dtype) #int32
+
+#creacion de array tipo float64
+arr2 = np.array(list, dtype='float64')
+print (arr2)
+print(arr2.dtype) #float64
+
+#Cambiar un array de tipo sin tener que crearlo
+arr = arr.astype(np.string_)
+print(arr)
+print(arr.dtype)
