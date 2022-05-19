6139831be2b9f320d28d3cd43f0e974044bc20f3
Harby Morales <ingharby@gmail.com>
2022-05-19 11:26:16 -0500

    FIN pruebas de lectura de archvios planos CSV y archivos JSON

--- Archives.py
+++ Archives.py
@@ -5,7 +5,22 @@ df_books = pd.read_csv("./Resources/bestsellers-with-categories.csv", sep=',', h
 print (df_books)
 
 
-#Guardo el data frame, con separador de ',' y que los headers estan en la fila 0
+#Guardo el data frame, le puedo cambiar los nombres de los encabezados
 df_books_2 = pd.read_csv("./Resources/bestsellers-with-categories.csv", sep=',', header=0,
 names=['Nombre', 'Autor', 'Critica', 'Reviews', 'Price', 'Year', 'Genre'])
 print (df_books_2)
+
+
+#Manejo de archivos JSON, son datos no estructurados llave valor
+#Para pasar de un archivo JSON que es no estructurado a un data frame que si es estructurado
+df_HP_Characters = pd.read_json('./Resources/HPCharactersDataRaw.json')
+print (df_HP_Characters)
+
+#Para pasar de un archivo JSON a formato raw
+df_HP_Characters_2 = pd.read_json('./Resources/HPCharactersDataRaw.json', typ='Series' )
+print (df_HP_Characters_2.info())
+
+
+#PRUEBAS DE FILTRADO CON LOC E ILOC
+
+
